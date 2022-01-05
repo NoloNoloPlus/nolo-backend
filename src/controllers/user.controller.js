7869967ff91464e7f5bc0f28ad88c6ccdaa2a33a
Samuele Marro <marrosamuele@gmail.com>
2022-01-05 16:25:30 +0100

    Added harmonization to user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,17 +3,18 @@ const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { userService } = require('../services');
+const { harmonizeResult } = require('../utils/misc');
 
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
-  res.status(httpStatus.CREATED).send(user);
+  res.status(httpStatus.CREATED).send(harmonizeResult(user));
 });
 
 const getUsers = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role']);
+  const filter = pick(req.query, ['email', 'role']);
   const options = pick(req.query, ['sortBy', 'limit', 'page']);
   const result = await userService.queryUsers(filter, options);
-  res.send(result);
+  res.send(harmonizeResult(result));
 });
 
 const getUser = catchAsync(async (req, res) => {
@@ -21,12 +22,12 @@ const getUser = catchAsync(async (req, res) => {
   if (!user) {
     throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
   }
-  res.send(user);
+  res.send(harmonizeResult(user));
 });
 
 const updateUser = catchAsync(async (req, res) => {
   const user = await userService.updateUserById(req.params.userId, req.body);
-  res.send(user);
+  res.send(harmonizeResult(user));
 });
 
 const deleteUser = catchAsync(async (req, res) => {
